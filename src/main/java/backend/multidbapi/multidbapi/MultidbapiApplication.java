1d0355443873f7585bfa42c5d03be6a8d02fb4b3
HalimBacic <33162351+HalimBacic@users.noreply.github.com>
2024-07-18 08:56:57 +0300

    API initialize
    Start project

---
+++ src/main/java/backend/multidbapi/multidbapi/MultidbapiApplication.java
@@ -0,0 +1,13 @@
+package backend.multidbapi.multidbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultidbapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/multidbapi/multidbapi/MultidbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.multidbapi.multidbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
